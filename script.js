1a23cd2b68897c7e6f549f5ccf3c13db573cbe39
Erin Mahoney <erinmahoneyphoto@gmail.com>
2015-09-19 13:04:52 -0700

    Fixing errors

--- script.js
+++ script.js
@@ -1,40 +1,8 @@
 window.onload = function() {
-
-  // array to store photo data
-  var photos = [
-		{ title: "This is a title", imgSrc: "http://rs65.pbsrc.com/albums/h235/Ignwar/Album%20Deserts/JoshuaTreeSunsetMojaveDesertCalifor.jpg~c200"},
-		{ title: "This is a title", imgSrc: "http://rs65.pbsrc.com/albums/h235/Ignwar/Album%20Deserts/JoshuaTreeSunsetMojaveDesertCalifor.jpg~c200"},
-		{ title: "This is a title", imgSrc: "http://rs65.pbsrc.com/albums/h235/Ignwar/Album%20Deserts/JoshuaTreeSunsetMojaveDesertCalifor.jpg~c200"},
-		{ title: "This is a title", imgSrc: "http://rs65.pbsrc.com/albums/h235/Ignwar/Album%20Deserts/JoshuaTreeSunsetMojaveDesertCalifor.jpg~c200"},
-		{ title: "This is a title", imgSrc: "http://rs65.pbsrc.com/albums/h235/Ignwar/Album%20Deserts/JoshuaTreeSunsetMojaveDesertCalifor.jpg~c200"},
-		{ title: "This is a title", imgSrc: "http://rs65.pbsrc.com/albums/h235/Ignwar/Album%20Deserts/JoshuaTreeSunsetMojaveDesertCalifor.jpg~c200"},
-		{ title: "This is a title", imgSrc: "http://rs65.pbsrc.com/albums/h235/Ignwar/Album%20Deserts/JoshuaTreeSunsetMojaveDesertCalifor.jpg~c200"},
-		{ title: "This is a title", imgSrc: "http://rs65.pbsrc.com/albums/h235/Ignwar/Album%20Deserts/JoshuaTreeSunsetMojaveDesertCalifor.jpg~c200"},
-		{ title: "This is a title", imgSrc: "http://rs65.pbsrc.com/albums/h235/Ignwar/Album%20Deserts/JoshuaTreeSunsetMojaveDesertCalifor.jpg~c200"},
-		{ title: "This is a title", imgSrc: "http://rs65.pbsrc.com/albums/h235/Ignwar/Album%20Deserts/JoshuaTreeSunsetMojaveDesertCalifor.jpg~c200"},
-  ];
-
   // access to div where photos will be appended
   var gallery = document.getElementById('gallery');
-
-  // iterate through array of photos
-  for(var i = 0; i < photos.length; i++) {
-		// set variable to photos[i]
-		var photo = photos[i];
-		// access to the photo-template created in html
-		var template = document.getElementById('photoTemplate').content.cloneNode(true);
-		// append title data to title id in template
-		template.querySelector('.title').innerText = photo.title;
-		// append imgSrc data to imgSrc id in template
-		template.querySelector('.imgSrc').src = photo.imgSrc;
-		// append the template data to div with gallery id
-		gallery.appendChild(template);
-  }
-
-  window.instagram_response = function(data) {
-		console.log(data);
-  };
-
+  // access to the photo-template created in html
+	var template = document.getElementById('photoTemplate').content.cloneNode(true);
 
   document.getElementById('tagSubmit').addEventListener('click', function(event) {
 		event.preventDefault();
@@ -49,7 +17,32 @@ window.onload = function() {
 		script.src = url;
 		document.body.appendChild(script);
 
+		document.getElementById('tagInput').value = "";
   });
+
+  var photoArr =[];
+  var photo;
+
+  window.instagram_response = function(data) {
+		var allPhotos = data;
+
+		// iterate through array of photos
+		for(var i = 0; i < allPhotos.data.length; i++) {
+
+			photo = allPhotos.data[i];
+			photoArr.push(photo);
+		}
+
+		for (var j = 0; j < photoArr.length; j++) {
+			console.log(photoArr);
+			// append title data to title id in template
+			template.querySelector('.title').innerText = photoArr[j].caption.text;
+			// append imgSrc data to imgSrc id in template
+			template.querySelector('.imgSrc').src = photoArr[j].images.low_resolution.url;
+			// append the template data to div with gallery id
+			gallery.appendChild(template);
+		}
+  };
 
 
 };
