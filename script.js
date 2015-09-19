65a1f9a1e84b2ab59d910857e330c7686295e525
Erin Mahoney <erinmahoneyphoto@gmail.com>
2015-09-19 15:31:00 -0700

    moved functions outside of onload

--- script.js
+++ script.js
@@ -1,9 +1,77 @@
+var photos, gallery, modalOverlay, leftModalButton, rightModalButton;
+
+var instagram_response = function(data) {
+	// clear the gallery
+	while (gallery.hasChildNodes()) {
+		gallery.removeChild(gallery.lastChild);
+	}
+
+	photos = data.data;
+
+	for(var i = 0; i < photos.length; i++) {
+		// access to the photo-template created in html
+		var template = document.getElementById('photoTemplate').content.cloneNode(true);
+		// append title data to title id in template
+		template.querySelector('.title').innerText = photos[i].caption.text;
+		// append imgSrc data to imgSrc id in template
+		template.querySelector('.imgSrc').src = photos[i].images.low_resolution.url;
+		// append data-index to anchor in template
+		template.querySelector('.imgSrc').setAttribute('data-index', i);
+		// append the template data to div with gallery id
+		gallery.appendChild(template);
+	}
+
+	addLightboxEventListeners();
+};
+
+var addLightboxEventListeners = function() {
+	var anchors = document.getElementsByClassName('lightboxAnchor');
+
+	for(var i = 0; i < anchors.length; i++) {
+		anchors[i].addEventListener('click', anchorClicked);
+	}
+};
+
+var anchorClicked = function(event) {
+	event.preventDefault();
+	// getting anchor
+	var anchor = event.target;
+	// getting data-index on image
+	var photoIndex = parseInt(anchor.getAttribute('data-index'), 10);
+	// set photoIndex for left and right anchor on modal
+	leftModalButton.setAttribute('data-index', photoIndex - 1);
+	rightModalButton.setAttribute('data-index', photoIndex + 1);
+	if (photoIndex === 0) {
+		leftModalButton.style.visibility = "hidden";
+	} else if (photoIndex === 19) {
+		rightModalButton.style.visibility = "hidden";
+	} else {
+		leftModalButton.style.visibility = "inherit";
+		rightModalButton.style.visibility = "inherit";
+	}
+	// show image at index
+	showModalImage(photoIndex);
+	// setting the modalOverlay visibility to visible
+	modalOverlay.style.visibility = 'visible';
+};
+
+var showModalImage = function(index) {
+	// getting photos standard_resolution url
+	var displayPhoto = photos[index].images.standard_resolution.url;
+	// getting photos caption
+	var displayTitle = photos[index].caption.text;
+	// setting the caption to modalTitle
+	document.getElementById('modalTitle').innerText = displayTitle;
+	// setting the image to the modalImg
+	document.getElementById('modalImg').src = displayPhoto;
+};
+
 window.onload = function() {
 	// access to div where photos will be appended
-	var gallery = document.getElementById('gallery');
-	var photos = [];
+	gallery = document.getElementById('gallery');
+
 	// getting context of modalOverlay
-	var modalOverlay = document.getElementsByClassName('modalOverlay')[0];
+	modalOverlay = document.getElementsByClassName('modalOverlay')[0];
 
 	document.getElementById('searchForm').addEventListener('submit', function(event) {
 		event.preventDefault();
@@ -36,76 +104,11 @@ window.onload = function() {
 		}
 	});
 
-	window.instagram_response = function(data) {
-		// clear the gallery
-		while (gallery.hasChildNodes()) {
-			gallery.removeChild(gallery.lastChild);
-		}
+	leftModalButton = document.getElementById('leftModalButton');
+	rightModalButton = document.getElementById('rightModalButton');
 
-		photos = data.data;
-
-		for(var i = 0; i < photos.length; i++) {
-			// access to the photo-template created in html
-			var template = document.getElementById('photoTemplate').content.cloneNode(true);
-			// append title data to title id in template
-			template.querySelector('.title').innerText = photos[i].caption.text;
-			// append imgSrc data to imgSrc id in template
-			template.querySelector('.imgSrc').src = photos[i].images.low_resolution.url;
-			// append data-index to anchor in template
-			template.querySelector('.imgSrc').setAttribute('data-index', i);
-			// append the template data to div with gallery id
-			gallery.appendChild(template);
-		}
-
-		addLightboxEventListeners();
-	};
-
-	window.addLightboxEventListeners = function() {
-		var anchors = document.getElementsByClassName('lightboxAnchor');
-
-		for(var i = 0; i < anchors.length; i++) {
-			anchors[i].addEventListener('click', anchorClicked);
-		}
-	};
-
-	window.anchorClicked = function(event) {
-		event.preventDefault();
-		// getting anchor
-		var anchor = event.target;
-		// getting data-index on image
-		var photoIndex = parseInt(anchor.getAttribute('data-index'), 10);
-		// set photoIndex for left and right anchor on modal
-		leftModalButton.setAttribute('data-index', photoIndex - 1);
-		rightModalButton.setAttribute('data-index', photoIndex + 1);
-		if (photoIndex === 0) {
-			leftModalButton.style.visibility = "hidden";
-		} else if (photoIndex === 19) {
-			rightModalButton.style.visibility = "hidden";
-		} else {
-			leftModalButton.style.visibility = "inherit";
-			rightModalButton.style.visibility = "inherit";
-		}
-		// show image at index
-		showModalImage(photoIndex);
-		// setting the modalOverlay visibility to visible
-		modalOverlay.style.visibility = 'visible';
-	};
-
-	var leftModalButton = document.getElementById('leftModalButton');
 	leftModalButton.addEventListener('click', anchorClicked);
-	var rightModalButton = document.getElementById('rightModalButton');
 	rightModalButton.addEventListener('click', anchorClicked);
-
-	window.showModalImage = function(index) {
-		// getting photos standard_resolution url
-		var displayPhoto = photos[index].images.standard_resolution.url;
-		// getting photos caption
-		var displayTitle = photos[index].caption.text;
-		// setting the caption to modalTitle
-		document.getElementById('modalTitle').innerText = displayTitle;
-		// setting the image to the modalImg
-		document.getElementById('modalImg').src = displayPhoto;
-	};
 };
 
 
